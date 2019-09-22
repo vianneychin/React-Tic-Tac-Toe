d2590b24eddb6f2584aafd76d0dbf53eff8e6a2e
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-09-22 03:45:39 -0700

    Add a handleClick function that will create a shallow copy of the state using the Array.prototype.slice() method. We will then replace the index number of the square with an X in lieu. Then we change the state of the squares with the shallow copy of the squares we edited with the added X

--- src/index.js
+++ src/index.js
@@ -14,18 +14,36 @@ class Square extends React.Component {
       <button
         className='square'
         onClick={() => {
-          this.setState({ value: 'X' })
+          this.props.onClick()
         }}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     )
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares: squares })
+  }
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i)
+        }}
+      />
+    )
   }
 
   render() {
